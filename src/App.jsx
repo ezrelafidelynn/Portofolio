7417fbf5edd39c72fe976f31cbe4302e9b81b895
Ezrela <ezre252@gmail.com>
2025-12-08 21:10:25 +0700

    Remove duplicate Projects section - now only appears once in Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import "./index.css";
 
@@ -15,9 +14,6 @@ function App() {
       <div>
         <section id="home">
           <Hero />
-        </section>
-        <section id="projects">
-          <Projects />
         </section>
         <section id="contact">
           <Contact />
